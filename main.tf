e1cf10fcd2440d024545bce7f5a71e4345a29405
julien-muke <julien.muke@gmail.com>
2024-05-05 13:22:30 +0200

    🚀 Create EC2 instance with Terraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami = "ami-07caf09b362be10b8"
+    instance_type = "t2.micro"
+    tags = {
+        "Name" = "MyEC2Instance"
+    }
+}
